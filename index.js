fab78e1f02654575d4820378f5921a55e4c9ff60
webioann <webioann@gmail.com>
2025-08-17 17:39:19 +0300

    learn write and read from files

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
-const {first} = require('goffy');
 const fs = require('fs');
 const path = require('path');
 
-let pathToDirectory = './modules';
-let pathToFile = './modules/first_module.js';
-
-// check if directory exists
-const checkDirectoryOrFileExists = (pathTo) => {
-    let result = fs.existsSync(pathTo);
-    if (!result) {
-        console.error(`Directory or file does not exist: ${pathTo}`);
-    }   
-    else {
-        console.log(`Directory or file exists: ${pathTo}`);
-    }
+// function for reading txt files
+const readTextFile = () => {
+    let textFrom = fs.readFileSync('./src/text.txt', {
+        encoding: 'utf8',
+        flag: 'r'
+    });
+    console.log(textFrom)
 }
 
-// output file stats information
-const fileStatsInfo = (arg_pathToFile) => {
-    let fileExists = fs.existsSync(arg_pathToFile);
-    let stats = null;
-    fileExists ? stats = fs.statSync(arg_pathToFile) : console.error(`File does not exist: ${arg_pathToFile}`);
-    stats && (console.log(`File size: ${stats.size} bytes.`), console.table(stats));
+// write to file
+const text = 'I am new text';
+const pathTo = './src/new222.txt';
+const pathTo2 = './src/newfile.txt';
+
+const writeTextIntoFile = (pathTo, newText) => {
+    fs.writeFileSync(pathTo, newText, {
+        encoding: 'utf8',
+        flag: 'a'
+    })
+    console.log('File written successfully');
 }
 
-// parse file and directory paths
-const parsePath = (arg_path) => {
-    let parsedPath = path.parse(arg_path);
-    console.table(parsedPath)
-}
+// write file from array
+const textArray = ['Hello', 'World', '.?????', 'This', 'is', 'a', 'new', 'text'];
 
-// get list all files in a directory
-const getListOfFilesInDirectory = (arg_pathToDirectory) => {
-    let files = fs.readdirSync(arg_pathToDirectory);
-    if (files.length === 0) {
-        console.log(`No files found in directory: ${arg_pathToDirectory}`);
-    } else {
-        console.log(`Files in directory ${arg_pathToDirectory}:`);
-        files.forEach(file => console.log(file));
-    }
+const writeArrayToFile = (pathTo, array) => {
+    fs.writeFileSync(pathTo, array.join(" "), {
+        encoding: 'utf8',
+        flag: 'a'
+    });
+    console.log('Array written to file successfully');
 }
 
 
-first()
-checkDirectoryOrFileExists(pathToDirectory)
-checkDirectoryOrFileExists(pathToFile)
-fileStatsInfo(pathToFile);
-parsePath(pathToFile);
-getListOfFilesInDirectory('../NodeJS_sandbox');
-
-// temporary code to test the module
-console.log(`Absolute path to directory: ${__dirname}`);
+readTextFile();
+writeTextIntoFile(pathTo, text);
+writeArrayToFile(pathTo2, textArray);

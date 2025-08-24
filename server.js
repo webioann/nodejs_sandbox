a7563517e726a6e935b23e8fce140bfa9e7367f1
webioann <webioann@gmail.com>
2025-08-24 19:15:10 +0300

    some works with POST method in Node js

--- server.js
+++ server.js
@@ -6,29 +6,29 @@ const PORT = 8383;
 // app.use(express.static('public'));
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    let url = req.url;
-    let method = req.method;
-    console.log(method, url);
-    res.send(`<h1>Hello from server again and again</h1>`);
-    res.sendStatus(200)
-    res.end();
-})
+// app.get('/', (req, res) => {
+//     let url = req.url;
+//     let method = req.method;
+//     console.log(method, url);
+//     res.send(`<h1>Hello from server again and again</h1>`);
+//     res.sendStatus(200)
+//     res.end();
+// })
 // API endpoint
-app.get('/api/data', (req, res) => {
-    const data = fs.readFileSync('./server/db/raw.json', 'utf8');
-    res.send(data); 
-    res.end();
-});
+// app.get('/api/data', (req, res) => {
+//     const data = fs.readFileSync('./server/db/raw.json', 'utf8');
+//     res.send(data); 
+//     res.end();
+// });
 // POST endpoint
 app.use(express.json());
-let name = { name: "Julia" }
+app.use(express.urlencoded({ extended: true }));
+
 app.post('/api/data', (req, res) => {
-    let body1 = req.body;
-    res.setHeaders('Content-Type', 'application/json');
-    name.push(body1.name);
+    let data = `, ${JSON.stringify(req.body)}`
+    fs.appendFileSync('./server/db/usersDB.js', data, 'utf8');
+    console.log(`Received name: ${data}`);
     res.sendStatus(201);
-    console.log(name);
     res.end();
 });
 
---
+++ server/db/usersDB.js
@@ -0,0 +1,6 @@
+[
+    {"age":30,"city":"New York"},
+    {"age":4,"city":"Kiev"},
+    {"age":48,"city":"Rivne"}
+]
+, {"age":18,"city":"Done"}, {"age":28,"city":"Donna"}, {"age":48,"city":"Danver"}

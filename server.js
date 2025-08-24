0c788b1324dcde1dcfdb2a2ece57e98d7aca82de
webioann <webioann@gmail.com>
2025-08-24 14:27:33 +0300

    start using nodemon and back HTML page from server on Express js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();      
 const PORT = 8383;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.use(express.static('public'));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    let url = req.url;
+    let method = req.method;
+    console.log(method, url);
+    res.send(`<h1>Hello from server again and again</h1>`);
+    res.sendStatus(200)
+    res.end();
+})
+
+app.listen(PORT);

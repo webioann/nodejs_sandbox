c948694340901d18ce48d0f27a228aea3baa527f
webioann <webioann@gmail.com>
2025-08-31 16:03:46 +0300

    GET method search and query params

---
+++ server/db/user.js
@@ -0,0 +1,6 @@
+export const data = [
+    {"uid":1,"age":48,"sex":"male","name":"David","email":"example@google.com"},
+    {"uid":2,"age":18,"sex":"male","name":"Stu","email":"example@google.com"},
+    {"uid":3,"age":34,"sex":"female","name":"Jody","email":"example@google.com"}
+]
+// export default data;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,77 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
 
 app.get('/', (req, res) => {
-    res.send('Hello, World from my new server!');
-    console.log(`Server is running on port ${PORT}`)
+    // let html = fs.readFileSync('./public/home.html', 'utf-8');
+    // res.send(<h1>Hello, World from my new server!</h1>);
+    // res.sendFile(path.join(__dirname, 'home.html'));
+    // res.sendStatus(200).send({msg: 'Hello, World from my new server!'});
+    // console.log(`Server is running on port ${PORT}`)
 });
+type User = {
+    uid: number
+    age: number
+    sex: 'male' | 'female'
+    name: string
+    email: string
+}
 
+const data: Array<User> = [
+    {"uid":1,"age":48,"sex":"male","name":"David","email":"example@google.com"},
+    {"uid":2,"age":18,"sex":"male","name":"Stu","email":"example@google.com"},
+    {"uid":3,"age":34,"sex":"female","name":"Jody","email":"example@google.com"}
+]
+
+// GET /api/users
+app.get('/api/users', (req, res) => {
+    res.send(data); 
+    res.sendStatus(200);
+})
+// GET /api/users/1
+app.get('/api/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    if (isNaN(userId)) {
+        return res.status(400).send({ error: 'Invalid user ID' });
+    }
+    else {
+        const user = data.find(u => u.uid === userId);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send({ error: 'User not found' });
+        }
+    }
+});
+// order Users by age
+// GET /api/users?sort=asc
+app.get('/api/usersOrder', (req, res) => {
+    type order = 'asc' | 'desc' 
+    let sortBy = req.query.sort as order | undefined;
+    if(sortBy === undefined) {res.status(404).send({ error: 'Order for sorting data not found' });}
+    else{
+        let sortedData = [...data];
+        if(sortBy === 'asc') {
+            sortedData.sort((a, b) => a.age - b.age);
+        } else if(sortBy === 'desc') {
+            sortedData.sort((a, b) => b.age - a.age );
+        }
+        res.json(sortedData);
+    }
+})
+// filter Users by name
+// GET /api/users?name=David
+app.get('/api/usersFilter', (req, res) => {
+    let nameFilter = req.query.name as string | undefined;
+    if(nameFilter === undefined) {res.status(404).send({ error: 'Name for filtering data not found' });}
+    else{
+        let filteredData = data.filter(user => user.name.toLowerCase() === nameFilter?.toLowerCase());
+        res.json(filteredData);
+    }
+})
